299b9c37e3d1bd4f665f71c5b8a3cd7532ee7143
Cedric <cedric.debot@gmail.com>
2023-04-16 20:51:26 +0200

    fix: removed node_modules & dist

--- dist/app.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const my_custom_error_1 = require("./my-custom-error");
-exports.getSomething = () => {
-    console.log('get something');
-    throw new my_custom_error_1.MyCustomError();
-    return 'Hello world';
-};
-//# sourceMappingURL=app.service.js.map
